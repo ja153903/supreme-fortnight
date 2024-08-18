66f0c3e2ba43f92820491d23a88e377c9a903045
ja153903 <jaimeabbariao@gmail.com>
2024-08-17 23:07:37 -0400

    adding leetcode problem 17

---
+++ leetcode/17.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "bun:test";
+import { letterCombinations } from "./17";
+
+test("leetcode 17. letter combinations of a phone number", () => {
+  expect(letterCombinations("")).toEqual([]);
+  expect(letterCombinations("2")).toEqual(["a", "b", "c"]);
+  expect(letterCombinations("23")).toEqual([
+    "ad",
+    "ae",
+    "af",
+    "bd",
+    "be",
+    "bf",
+    "cd",
+    "ce",
+    "cf",
+  ]);
+});
