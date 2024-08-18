0f25969abb4c8f4944c2f467547826b092d55d27
ja153903 <jaimeabbariao@gmail.com>
2024-08-17 23:20:22 -0400

    adding leetcode 22 generateParenthesis

---
+++ leetcode/22.test.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "bun:test";
+import { generateParenthesis } from "./22";
+
+test("leetcode 22. generate parenthesis", () => {
+  expect(generateParenthesis(1)).toEqual(["()"]);
+  expect(generateParenthesis(3)).toEqual([
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()",
+  ]);
+});
