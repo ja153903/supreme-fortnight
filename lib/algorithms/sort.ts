a04d925237f2a31211963966ac398f31608f33ce
ja153903 <jaimeabbariao@gmail.com>
2024-10-05 09:49:43 -0400

    adding docstring for ssort

--- lib/algorithms/sort.ts
+++ lib/algorithms/sort.ts
@@ -1,3 +1,7 @@
+/**
+ * `ssort` sorts strings in TypeScript by splitting the string by character
+ * sorting each character and then joining everything back
+ */
 export function ssort(s: string): string {
   return s
     .split("")
