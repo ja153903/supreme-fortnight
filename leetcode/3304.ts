51e639da5f680569cc641138899df015b61e6681
ja153903 <jaimeabbariao@gmail.com>
2024-10-05 13:57:44 -0400

    updating with some contest problems

---
+++ leetcode/3304.ts
@@ -0,0 +1,19 @@
+import { CIRCULAR_ALPHABET } from "@utils/alphabet";
+
+function kthCharacter(k: number): string {
+  let word = ["a"];
+
+  while (word.length < k) {
+    const size = word.length;
+
+    for (let i = 0; i < size; i++) {
+      const index = CIRCULAR_ALPHABET.indexOf(word[i]);
+      const nextIndex = (index + 1) % 26;
+      word.push(CIRCULAR_ALPHABET[nextIndex]);
+    }
+  }
+
+  return word?.[k - 1] ?? "";
+}
+
+export { kthCharacter };
---
+++ utils/alphabet.ts
@@ -0,0 +1,80 @@
+import { Counter } from "@lib/data-structures/counter";
+
+export const CIRCULAR_ALPHABET = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export function isVowel(char: string): boolean {
+  return (
+    char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+  );
+}
+
+export function isConsonant(char: string): boolean {
+  return !isVowel(char);
+}
+
+export function hasAtLeastOneVowel(word: string): boolean {
+  for (const char of word) {
+    if (isVowel(char)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export function hasEveryVowel(word: string): boolean {
+  const counter = new Counter<string>("aeiou");
+
+  for (const char of word) {
+    if (counter.has(char)) {
+      counter.decrement(char);
+    }
+  }
+
+  for (const count of counter.values()) {
+    if (count > 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function getConsonantCount(word: string): number {
+  let count = 0;
+
+  for (const char of word) {
+    if (isConsonant(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
