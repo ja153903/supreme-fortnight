bee7cf8a3941ab9570b3236618e011b2c035eba6
ja153903 <jaimeabbariao@gmail.com>
2024-10-25 00:34:13 -0400

    solving leetcode daily

---
+++ leetcode/1233.test.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "bun:test";
+import { removeSubfolders } from "./1233";
+
+test("1233. Remove Sub-Folders from the Filesystem", () => {
+  expect(removeSubfolders(["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])).toEqual([
+    "/a",
+    "/c/d",
+    "/c/f",
+  ]);
+  expect(removeSubfolders(["/a/b/c", "/a/b/ca", "/a/b/d"])).toEqual([
+    "/a/b/c",
+    "/a/b/ca",
+    "/a/b/d",
+  ]);
+});
