db90cc96e74846b4644606471c2961f10a9d88fa
ja153903 <jaimeabbariao@gmail.com>
2024-12-28 20:11:53 -0500

    started to solve some 2015 problems again

---
+++ advent-of-code/2015/day1/main.ts
@@ -0,0 +1,40 @@
+import { readInputToString } from "@utils/advent-of-code";
+
+const data = await readInputToString(`${import.meta.dir}/data.in`);
+
+function part1() {
+	let res = 0;
+
+	for (let i = 0; i < data.length; i++) {
+		if (data[i] === "(") {
+			res++;
+		}
+		if (data[i] === ")") {
+			res--;
+		}
+	}
+
+	return res;
+}
+
+function part2() {
+	let level = 0;
+
+	for (let i = 0; i < data.length; i++) {
+		if (data[i] === "(") {
+			level++;
+		}
+		if (data[i] === ")") {
+			level--;
+		}
+
+		if (level < 0) {
+			return i + 1;
+		}
+	}
+
+	throw new Error("Santa never enters the basement");
+}
+
+console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
---
+++ advent-of-code/2015/day3/main.ts
@@ -0,0 +1,78 @@
+import { readInputToString } from "@utils/advent-of-code";
+
+const data = await readInputToString(`${import.meta.dir}/data.in`);
+
+function part1() {
+	const visited = new Set<string>();
+	let x = 0;
+	let y = 0;
+
+	visited.add(`${x},${y}`);
+
+	for (let i = 0; i < data.length; i++) {
+		if (data[i] === "^") {
+			y -= 1;
+		}
+		if (data[i] === "v") {
+			y += 1;
+		}
+		if (data[i] === "<") {
+			x -= 1;
+		}
+		if (data[i] === ">") {
+			x += 1;
+		}
+		visited.add(`${x},${y}`);
+	}
+
+	return visited.size;
+}
+
+function part2() {
+	const visited = new Set<string>();
+	let x = 0;
+	let y = 0;
+	let rx = 0;
+	let ry = 0;
+
+	visited.add(`${x},${y}`);
+
+	for (let i = 0; i < data.length; i++) {
+		if (data[i] === "^") {
+			if (i % 2 === 0) {
+				ry -= 1;
+			} else {
+				y -= 1;
+			}
+		}
+		if (data[i] === "v") {
+			if (i % 2 === 0) {
+				ry += 1;
+			} else {
+				y += 1;
+			}
+		}
+		if (data[i] === "<") {
+			if (i % 2 === 0) {
+				rx -= 1;
+			} else {
+				x -= 1;
+			}
+		}
+		if (data[i] === ">") {
+			if (i % 2 === 0) {
+				rx += 1;
+			} else {
+				x += 1;
+			}
+		}
+
+		visited.add(`${x},${y}`);
+		visited.add(`${rx},${ry}`);
+	}
+
+	return visited.size;
+}
+
+console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
