49fe3cadd51a5d95be8f275a4c7a4f7252976243
ja153903 <jaimeabbariao@gmail.com>
2024-12-29 21:37:22 -0500

    finishing solution for day 7 2015

--- advent-of-code/2015/day7/main.ts
+++ advent-of-code/2015/day7/main.ts
@@ -100,15 +100,118 @@ function part1() {
 				break;
 			}
 			case "AND": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber & rhsAsNumber);
+
 				break;
 			}
 			case "OR": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber | rhsAsNumber);
+
 				break;
 			}
 			case "RSHIFT": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber >> rhsAsNumber);
+
 				break;
 			}
 			case "LSHIFT": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber << rhsAsNumber);
 				break;
 			}
 		}
@@ -117,7 +220,182 @@ function part1() {
 	return dd.get("a");
 }
 
-function part2() {}
+function part2() {
+	const dd = new Map<string, number>();
+
+	const indexToModify = instructions.findIndex(
+		({ register }) => register === "b",
+	);
+
+	const queue = structuredClone(instructions);
+
+	queue[indexToModify] = {
+		...queue[indexToModify],
+		lhs: "46065",
+	};
+
+	while (queue.length > 0) {
+		const front = queue.shift();
+		if (!front) {
+			throw new Error("Something went wrong with our queue");
+		}
+
+		const { operation, lhs, rhs, register } = front;
+
+		// TODO: Finish solving this problem
+		switch (operation) {
+			case "ASSIGN": {
+				// check if the lhs is a number
+				const asNumber = Number.parseInt(lhs);
+				if (Number.isNaN(asNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+					} else {
+						dd.set(register, existingValue);
+					}
+				} else {
+					dd.set(register, asNumber);
+				}
+
+				break;
+			}
+			case "NOT": {
+				// check if the lhs is a number
+				const asNumber = Number.parseInt(lhs);
+				if (Number.isNaN(asNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+					} else {
+						dd.set(register, ~existingValue);
+					}
+				} else {
+					dd.set(register, ~asNumber);
+				}
+
+				break;
+			}
+			case "AND": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber & rhsAsNumber);
+
+				break;
+			}
+			case "OR": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber | rhsAsNumber);
+
+				break;
+			}
+			case "RSHIFT": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber >> rhsAsNumber);
+
+				break;
+			}
+			case "LSHIFT": {
+				let lhsAsNumber = Number.parseInt(lhs);
+				if (Number.isNaN(lhsAsNumber)) {
+					const existingValue = dd.get(lhs);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					lhsAsNumber = existingValue;
+				}
+
+				// biome-ignore lint/style/noNonNullAssertion: <explanation>
+				let rhsAsNumber = Number.parseInt(rhs!);
+				if (Number.isNaN(rhsAsNumber)) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					const existingValue = dd.get(rhs!);
+					if (existingValue == null) {
+						queue.push(front);
+						break;
+					}
+
+					rhsAsNumber = existingValue;
+				}
+
+				dd.set(register, lhsAsNumber << rhsAsNumber);
+				break;
+			}
+		}
+	}
+
+	return dd.get("a");
+}
 
 console.log(`Part 1: ${part1()}`);
 console.log(`Part 2: ${part2()}`);
