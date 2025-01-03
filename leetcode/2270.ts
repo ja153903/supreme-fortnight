5629c7e3066c8e3cb241645007f3944f32852fa2
ja153903 <jaimeabbariao@gmail.com>
2025-01-02 19:35:36 -0500

    daily leetcode 2025 day 3

---
+++ leetcode/2270.ts
@@ -0,0 +1,23 @@
+function waysToSplitArray(nums: number[]): number {
+	const prefix = new Array<number>(nums.length).fill(0);
+
+	for (let i = 0; i < nums.length; i++) {
+		if (i > 0) {
+			prefix[i] += prefix[i - 1];
+		}
+
+		prefix[i] += nums[i];
+	}
+
+	let res = 0;
+
+	for (let i = 0; i < prefix.length - 1; i++) {
+		if (prefix[i] >= prefix[prefix.length - 1] - prefix[i]) {
+			res++;
+		}
+	}
+
+	return res;
+}
+
+export { waysToSplitArray };
