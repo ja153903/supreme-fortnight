3c57bbc9b62ab5f35acbbe2a14c90108befa3bdf
ja153903 <jaimeabbariao@gmail.com>
2025-01-20 16:05:40 -0500

    updating with a problem

---
+++ leetcode/654.ts
@@ -0,0 +1,26 @@
+import { TreeNode } from "@lib/data-structures/leetcode";
+
+function constructMaximumBinaryTree(nums: number[]): TreeNode | null {
+	if (nums.length === 0) {
+		return null;
+	}
+
+	// find the max value in the array
+	let maxIndex = 0;
+	let maxValue = Number.NEGATIVE_INFINITY;
+
+	nums.forEach((num, index) => {
+		if (num > maxValue) {
+			maxIndex = index;
+			maxValue = num;
+		}
+	});
+
+	const root = new TreeNode(maxValue);
+	root.left = constructMaximumBinaryTree(nums.slice(0, maxIndex));
+	root.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1));
+
+	return root;
+}
+
+export { constructMaximumBinaryTree };
