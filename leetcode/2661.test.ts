81cc9ebae0387fef752d7ceb2ec322a53a29f218
ja153903 <jaimeabbariao@gmail.com>
2025-01-19 20:18:09 -0500

    solving some problems

---
+++ leetcode/2661.test.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "bun:test";
+import { firstCompleteIndex } from "./2661";
+
+test("First Completely Painted Row or Column", () => {
+	const result = firstCompleteIndex(
+		[1, 4, 5, 2, 6, 3],
+		[
+			[4, 3, 5],
+			[1, 2, 6],
+		],
+	);
+	expect(result).toBe(1);
+});
---
+++ leetcode/2661.ts
@@ -0,0 +1,43 @@
+type Coordinate = { row: number; col: number };
+
+function firstCompleteIndex(arr: number[], mat: number[][]): number {
+	// go through each index i in arr starting from index 0 and paint
+	// the cell in mat containing the integer arr[i]
+
+	const ROWS = mat.length;
+	const COLS = mat[0].length;
+
+	// the idea here is that we want to keep track of when our hash
+	// has enough values for a given row or col
+	// For example, if we color (0, 0), then we should add count[row0]++ and count[col0]++
+
+	const counter = new Map<string, number>();
+	const positions = new Map<number, Coordinate>();
+
+	for (let row = 0; row < ROWS; row++) {
+		for (let col = 0; col < COLS; col++) {
+			positions.set(mat[row][col], { row, col });
+		}
+	}
+
+	for (let i = 0; i < arr.length; i++) {
+		const { row, col } = positions.get(arr[i])!;
+
+		const downTheCol = `r${row}`;
+		const acrossTheRow = `c${col}`;
+
+		counter.set(acrossTheRow, (counter.get(acrossTheRow) ?? 0) + 1);
+		counter.set(downTheCol, (counter.get(downTheCol) ?? 0) + 1);
+
+		if (
+			counter.get(acrossTheRow) === ROWS ||
+			counter.get(downTheCol) === COLS
+		) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+export { firstCompleteIndex };
